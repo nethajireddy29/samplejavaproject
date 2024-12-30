deda95225f444a67d8d36bd448300698a7af1e07
nethaji <nethaji8919@gmail.com>
2024-12-30 21:42:39 +0530

    c

---
+++ src/main/java/com/app/kmit/qwa/App.java
@@ -0,0 +1,13 @@
+package com.app.kmit.qwa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
